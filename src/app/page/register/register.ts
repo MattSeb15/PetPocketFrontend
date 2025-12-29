d930de2b4fdccd9ffd5d062b8780b16da0a162ee
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-12-28 22:23:34 -0500

    feat: Mejorar diseño de las páginas de inicio de sesión y registro con ajustes en CSS y HTML

--- src/app/page/register/register.ts
+++ src/app/page/register/register.ts
@@ -2,12 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
